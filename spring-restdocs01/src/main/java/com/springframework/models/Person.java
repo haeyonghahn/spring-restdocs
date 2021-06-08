dd42215ad3fdbbd74033758392361f06a96cfedc
haeyong Hahn <yong80211@gmail.com>
2021-06-08 16:53:38 +0900

    spring restDocs 테스트

---
+++ spring-restdocs01/src/main/java/com/springframework/models/Person.java
@@ -0,0 +1,28 @@
+package com.springframework.models;
+
+public class Person {
+
+	private String name;
+	private String message;
+	
+	public Person(String name, String message) {
+		super();
+		this.name = name;
+		this.message = message;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+	
+}
